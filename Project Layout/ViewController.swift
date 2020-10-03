d2496314fc3840fae178153320f9210fec787653
Yurii <Yurii>
2020-10-03 16:06:33 +0200

    Initial commit

---
+++ Project Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Layout
+//
+//  Created by Yurii Sameliuk on 31/01/2020.
+//  Copyright © 2020 Yurii Sameliuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
